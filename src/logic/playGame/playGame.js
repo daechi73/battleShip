bb022a465c9413368ac3204946b08c167105fbbc
J <59581793+daechi73@users.noreply.github.com>
2023-05-19 16:19:57 -0400

    playgame will call choosingUI rather than renderBoard

--- src/logic/playGame/playGame.js
+++ src/logic/playGame/playGame.js
@@ -2,22 +2,26 @@ import gameBoard from "../gameBoard/gameBoard";
 import player from "../player/computer/player";
 import computer from "../player/computer/computer";
 import ship from "../ship/ship";
-import {
-  renderPlayerBoard,
-  renderPlayer2Board,
-} from "../../render/renderBoard";
+import { choosingUI } from "../../render/renderUI";
 
-const playGame = (playerName) => {
-  const playerBoard = gameBoard();
-  const player2Board = gameBoard();
-  const player1 = player(playerName);
-  const player2 = computer();
+const gamePieces = () => {
   const carrier = ship("carrier", 5);
   const battleShip = ship("battleShip", 4);
   const cruiser = ship("cruiser", 3);
   const submarine = ship("submarine", 3);
   const destroyer = ship("destroyer", 2);
   const scout = ship("scout", 2);
+  return [carrier, battleShip, cruiser, submarine, destroyer, scout];
+};
+
+const playGame = (playerName) => {
+  const playerBoard = gameBoard();
+  const player2Board = gameBoard();
+  const player1 = player(playerName);
+  const player2 = computer();
+  const ships = gamePieces();
+
+  choosingUI(playerBoard, ships);
 };
 
 export default playGame;
