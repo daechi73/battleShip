1b22d5a4419bf2d8384c070b11940116c9e63c39
J <59581793+daechi73@users.noreply.github.com>
2023-05-20 23:40:20 -0400

    added draggable true to ship divs for horizontals

--- src/render/renderShip.js
+++ src/render/renderShip.js
@@ -1,13 +1,19 @@
 const renderShip = (ship) => {
   const boat = document.createElement("div");
   boat.classList.add(`ship`);
+  boat.id = `${ship.getName()}`;
   boat.dataset.name = `${ship.getName()}`;
+
   if (ship.getPosition() === "horizontal") {
-    boat.style.width = `${ship.getLength() * 35}px`;
+    boat.style.width = `${ship.getLength() * 38}px`;
     boat.style.height = "35px";
+    //boat.innerHTML = `<div class="shipName">${ship.getName()}</div>`;
+    boat.textContent = `${ship.getName()}`;
+    boat.draggable = "true";
   } else {
-    boat.style.width = "35px";
+    boat.style.width = "37px";
     boat.style.height = `${ship.length * 35}px`;
+    boat.draggable = "true";
   }
   return boat;
 };
