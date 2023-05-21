2f67a948f9e37ad42cbb75cb88f755aa8a96f09e
J <59581793+daechi73@users.noreply.github.com>
2023-05-21 01:32:07 -0400

    changed boat width

--- src/render/renderShip.js
+++ src/render/renderShip.js
@@ -2,13 +2,24 @@ const renderShip = (ship) => {
   const boat = document.createElement("div");
   boat.classList.add(`ship`);
   boat.id = `${ship.getName()}`;
-  boat.dataset.name = `${ship.getName()}`;
-
+  //boat.dataset.name = `${ship.getName()}`;
   if (ship.getPosition() === "horizontal") {
-    boat.style.width = `${ship.getLength() * 38}px`;
+    for (let i = 1; i <= ship.getLength(); i++) {
+      const shipSubset = document.createElement("div");
+      shipSubset.classList.add("shipSubset");
+      shipSubset.dataset.subset = `${i}`;
+      shipSubset.style.cssText = `
+        height:35px;
+        width:38px;
+      `;
+      console.log(shipSubset);
+      boat.appendChild(shipSubset);
+    }
+
+    boat.style.width = `${ship.getLength() * 36}px`;
     boat.style.height = "35px";
     //boat.innerHTML = `<div class="shipName">${ship.getName()}</div>`;
-    boat.textContent = `${ship.getName()}`;
+    //boat.textContent = `${ship.getName()}`;
     boat.draggable = "true";
   } else {
     boat.style.width = "37px";
