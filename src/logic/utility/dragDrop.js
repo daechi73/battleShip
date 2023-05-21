43b4d5ca5bc4a17fd14d2fe03f4e6ba92716b9a4
J <59581793+daechi73@users.noreply.github.com>
2023-05-20 23:41:47 -0400

    drags ships on the board

---
+++ src/logic/utility/dragDrop.js
@@ -0,0 +1,33 @@
+const dragEvent = () => {
+  let dragged = null;
+
+  const ships = document.querySelectorAll(".ship");
+  const cells = document.querySelectorAll(".cell");
+
+  ships.forEach((ship) => {
+    ship.addEventListener("dragstart", (e) => {
+      console.log("dragstarting");
+      console.log(e.target);
+      return (dragged = e.target);
+    });
+
+    ship.addEventListener("dragend", (e) => {
+      return "drag ending";
+    });
+    cells.forEach((cell) => {
+      cell.addEventListener("dragover", (e) => {
+        return e.preventDefault();
+      });
+    });
+    document.addEventListener("drop", (e) => {
+      e.preventDefault();
+      //console.log(e.target);
+      if (e.target.classList.contains("droppable")) {
+        dragged.parentNode.removeChild(dragged);
+        return e.target.appendChild(dragged);
+      }
+    });
+  });
+};
+
+export default dragEvent;
