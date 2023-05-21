394774497b98fe4863ee896753892e5f7b5fb533
J <59581793+daechi73@users.noreply.github.com>
2023-05-20 23:38:43 -0400

    didnt change anything; played around with codes to make dragging ships work; in the end nothing had to be changed here

--- src/render/renderPort.js
+++ src/render/renderPort.js
@@ -2,19 +2,46 @@ import renderShip from "./renderShip.js";
 const renderPort = (ships) => {
   const portContainer = document.createElement("div");
   const port = document.createElement("div");
+
   portContainer.classList.add("container-port");
   port.classList.add("port");
 
-  ships.forEach((ship) => {
+  //
+  // const randomDiv = document.createElement("div");
+  // const randomDIvDiv = document.createElement("div");
+  // randomDiv.classList.add("randomDiv");
+  // randomDiv.textContent = "randomDiv";
+  // randomDiv.draggable = "true";
+  // randomDiv.style.cssText = `
+  //   height: 35px;
+  //   width: 76px;
+  //   background-color:black;
+  // `;
+  // randomDIvDiv.appendChild(randomDiv);
+  // port.appendChild(randomDIvDiv);
+  //
+
+  // main
+  //
+  ships.forEach((ship, i) => {
     const shipDock = document.createElement("div");
     const boat = renderShip(ship);
     shipDock.classList.add("dock");
+    shipDock.classList.add("droppable");
     shipDock.appendChild(boat);
     port.appendChild(shipDock);
   });
 
   portContainer.appendChild(port);
   document.querySelector(".container-main").appendChild(portContainer);
+  //
+
+  // const boat = renderShip(ships[0]);
+  // //boat.draggable = "true";
+  // port.appendChild(boat);
+
+  // portContainer.appendChild(port);
+  // document.querySelector(".container-main").appendChild(portContainer);
 };
 
 export { renderPort };
