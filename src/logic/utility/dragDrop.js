80c739fd17f2abda26dd0f73647bf64c93cfe711
J <59581793+daechi73@users.noreply.github.com>
2023-05-21 10:47:51 -0400

    fixed errors: 1.only .ships can be dragged; 2.mousedoen event is specified only for .shipSubset;

--- src/logic/utility/dragDrop.js
+++ src/logic/utility/dragDrop.js
@@ -4,35 +4,49 @@ const dragEvent = () => {
 
   const ships = document.querySelectorAll(".ship");
   const cells = document.querySelectorAll(".cell");
-
+  //console.log(cells);
   ships.forEach((ship) => {
     ship.addEventListener("mousedown", (e) => {
-      shipSubset = e.target.dataset.subset;
-      console.log(e.target);
+      if (e.target.className === "shipSubset") {
+        shipSubset = e.target.dataset.subset;
+        console.log(e.target);
+      }
     });
     ship.addEventListener("dragstart", (e) => {
       console.log("dragstarting");
       console.log(e.target);
-      return (dragged = e.target);
+      console.log(`shipSubset= ${shipSubset}`);
+      if (e.target.className === "ship") return (dragged = e.target);
     });
 
     ship.addEventListener("dragend", (e) => {
       return "drag ending";
     });
-    cells.forEach((cell) => {
-      cell.addEventListener("dragover", (e) => {
-        return e.preventDefault();
-      });
+  });
+  cells.forEach((cell) => {
+    cell.addEventListener("dragover", (e) => {
+      return e.preventDefault();
     });
-    cells.forEach((cell) => {
-      cell.addEventListener("drop", (e) => {
-        e.preventDefault();
-        //console.log(e.target);
-        if (e.target.classList.contains("droppable")) {
-          dragged.parentNode.removeChild(dragged);
-          return e.target.appendChild(dragged);
-        }
-      });
+  });
+  cells.forEach((cell) => {
+    cell.addEventListener("drop", (e) => {
+      e.preventDefault();
+
+      console.log(cell);
+      if (e.target.classList.contains("droppable")) {
+        dragged.parentNode.removeChild(dragged);
+        // console.log(
+        //   document.querySelector(
+        //     `[data-column="${e.target.dataset.column - (shipSubset - 1)}"]`
+        //   )
+        // );
+        const realCellToAppend = document.querySelector(
+          `[data-column="${
+            e.target.dataset.column - (shipSubset - 1)
+          }"][data-row="${e.target.dataset.row}"]`
+        );
+        return realCellToAppend.appendChild(dragged);
+      }
     });
   });
 };
