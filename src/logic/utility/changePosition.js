cbe7d672e578ab4a4aff2f71f44c0cc751ccbee9
J <59581793+daechi73@users.noreply.github.com>
2023-06-13 14:53:57 -0400

    recalling eventhandlers on all objects when changing position implemented

--- src/logic/utility/changePosition.js
+++ src/logic/utility/changePosition.js
@@ -2,39 +2,31 @@ import renderShip from "../../render/renderShip";
 import dragEvent from "./dragDrop";
 const changePositionListener = (board, ships, dragEventObject) => {
   const shipSubsets = document.querySelectorAll(".shipSubset");
-  console.log(dragEventObject);
   shipSubsets.forEach((shipSubset) => {
     shipSubset.addEventListener("click", (e) => {
-      changePosition(ships, e);
+      changePosition(ships, e, dragEventObject);
     });
   });
 };
-const changePosition = (ships, e) => {
+const changePosition = (ships, e, dragEventObject) => {
   const shipContainer = e.target.parentNode.parentNode;
   console.log(e.target.parentNode.parentNode);
-  console.log("clicking");
   if (shipContainer.classList.contains("dock")) {
-    console.log("changing position");
-    console.log(e.target.parentNode.id);
     const ship = ships.find(
       (ship) => ship.getName() === e.target.parentNode.id
     );
     ship.changePosition();
-    console.log(ship.getPosition());
     e.target.parentNode.remove();
     const newShip = renderShip(ship);
     shipContainer.appendChild(newShip);
+
+    dragEventObject.recallDragEvents();
+
     newShip.childNodes.forEach((child) => {
       child.addEventListener("click", (event) => {
-        changePosition(ships, event);
+        changePosition(ships, event, dragEventObject);
       });
     });
-
-    // if (dragEventObject) {
-    //   dragEventObject.removeEvents();
-    //   console.log("existing..");
-    // } else newDragEvent.removeEvents();
-    // const newDragEvent = dragEvent();
   }
 };
 
