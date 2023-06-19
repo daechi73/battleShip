b4bfd79b394bc0bb0bedf3be551cbd03f17a9da1
J <59581793+daechi73@users.noreply.github.com>
2023-06-19 17:46:35 -0400

    implemented backend ship placement and position change mirroring front end control

--- src/logic/utility/changePosition.js
+++ src/logic/utility/changePosition.js
@@ -1,21 +1,20 @@
 import renderShip from "../../render/renderShip";
 import positionUtility from "./positionUtility";
 
-const changePositionListener = (board, ships, dragEventObject) => {
+const changePositionListener = (board, dragEventObject) => {
   const shipSubsets = document.querySelectorAll(".shipSubset");
   shipSubsets.forEach((shipSubset) => {
     shipSubset.addEventListener("click", (e) => {
-      changePosition(ships, e, dragEventObject);
+      changePosition(board, e, dragEventObject);
     });
   });
 };
-const changePosition = (ships, e, dragEventObject) => {
+const changePosition = (board, e, dragEventObject) => {
   const shipContainer = e.target.parentNode.parentNode;
   console.log(e.target.parentNode.parentNode);
   if (shipContainer.classList.contains("dock")) {
-    const ship = ships.find(
-      (ship) => ship.getName() === e.target.parentNode.id
-    );
+    const ship = board.findShip(e.target.parentNode.id);
+
     ship.changePosition();
     e.target.parentNode.remove();
     const newShip = renderShip(ship);
@@ -25,15 +24,14 @@ const changePosition = (ships, e, dragEventObject) => {
 
     newShip.childNodes.forEach((child) => {
       child.addEventListener("click", (event) => {
-        changePosition(ships, event, dragEventObject);
+        changePosition(board, event, dragEventObject);
       });
     });
   }
   if (shipContainer.classList.contains("cell")) {
-    const ship = ships.find(
-      (ship) => ship.getName() === e.target.parentNode.id
-    );
-    ship.changePosition();
+    const ship = board.findShip(e.target.parentNode.id);
+    board.turnShip(ship.getName());
+    board.printBoard();
     const newShip = renderShip(ship);
     if (positionUtility().checkBoundary(shipContainer, newShip)) {
       if (
@@ -50,7 +48,7 @@ const changePosition = (ships, e, dragEventObject) => {
 
     newShip.childNodes.forEach((child) => {
       child.addEventListener("click", (event) => {
-        changePosition(ships, event, dragEventObject);
+        changePosition(board, event, dragEventObject);
       });
     });
   }
