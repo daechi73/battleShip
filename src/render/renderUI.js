c2c2cc8cb3430c782ba0c041c405632e7636a940
J <59581793+daechi73@users.noreply.github.com>
2023-06-19 18:05:39 -0400

    removed all ships parameter as it is part of gameboard object

--- src/render/renderUI.js
+++ src/render/renderUI.js
@@ -3,17 +3,12 @@ import { renderPort } from "./renderPort.js";
 import dragEvent from "../logic/utility/dragDrop.js";
 import changePositionListener from "../logic/utility/changePosition";
 
-const choosingUI = (board, ships) => {
+const choosingUI = (board) => {
   renderPlayer1Side(board);
-  renderPort(ships);
-  //dragEvent();
-  const dragEventActivate = dragEvent();
-  changePositionListener(board, ships, dragEventActivate);
-
-  // const btn = document.createElement("button");
-  // btn.textContent = "btn";
-  // btn.onclick = dragEventActivate.removeEvents;
-  // document.body.appendChild(btn);
+  renderPort(board.getShips());
+  const dragEventActivate = dragEvent(board);
+  dragEventActivate.addEvents();
+  changePositionListener(board, dragEventActivate);
 };
 
 const gameUi = () => {};
