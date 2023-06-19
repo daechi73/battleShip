88433cbd257d8a282fa94f610412656eca3eec29
J <59581793+daechi73@users.noreply.github.com>
2023-06-19 17:43:15 -0400

    removed list of ships

--- src/logic/playGame/playGame.js
+++ src/logic/playGame/playGame.js
@@ -1,27 +1,15 @@
 import gameBoard from "../gameBoard/gameBoard";
 import player from "../player/computer/player";
 import computer from "../player/computer/computer";
-import ship from "../ship/ship";
 import { choosingUI } from "../../render/renderUI";
-
-const gamePieces = () => {
-  const carrier = ship("carrier", 5);
-  const battleShip = ship("battleShip", 4);
-  const cruiser = ship("cruiser", 3);
-  const submarine = ship("submarine", 3);
-  const destroyer = ship("destroyer", 2);
-  const scout = ship("scout", 2, "vertical");
-  return [carrier, battleShip, cruiser, submarine, destroyer, scout];
-};
 
 const playGame = (playerName) => {
   const playerBoard = gameBoard();
   const player2Board = gameBoard();
   const player1 = player(playerName);
   const player2 = computer();
-  const ships = gamePieces();
 
-  choosingUI(playerBoard, ships);
+  choosingUI(playerBoard);
 };
 
 export default playGame;
