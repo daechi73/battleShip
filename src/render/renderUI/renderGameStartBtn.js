5a746a582f224fda1ebec79184a4dc0f647c8ef2
J <59581793+daechi73@users.noreply.github.com>
2023-06-22 02:32:18 -0400

    added dragEventObject as parameter so it can remove drag events when gamestart button is clicked

--- src/render/renderUI/renderGameStartBtn.js
+++ src/render/renderUI/renderGameStartBtn.js
@@ -1,6 +1,6 @@
 import { renderPlayer2Side } from "./renderPlayerSides";
 import autoPcBoard from "../../logic/utility/autoPcBoard";
-const renderGameStartBtn = (player2Board) => {
+const renderGameStartBtn = (player2Board, dragEventObject) => {
   if (document.querySelector(".port")) {
     document.querySelector(".port").remove();
 
@@ -8,14 +8,19 @@ const renderGameStartBtn = (player2Board) => {
     gameStartBtn.textContent = "Game Start";
     gameStartBtn.classList.add("gameStartBtn");
     document.querySelector(".container-port").appendChild(gameStartBtn);
-    gameStartBtnEventListener(gameStartBtn, player2Board);
+    gameStartBtnEventListener(gameStartBtn, player2Board, dragEventObject);
   }
 };
-const gameStartBtnEventListener = (gameStartBtn, player2Board) => {
+const gameStartBtnEventListener = (
+  gameStartBtn,
+  player2Board,
+  dragEventObject
+) => {
   gameStartBtn.addEventListener("click", () => {
     document.querySelector(".container-port").remove();
     autoPcBoard(player2Board);
     renderPlayer2Side(player2Board);
+    dragEventObject.removeEvents();
   });
 };
 export default renderGameStartBtn;
