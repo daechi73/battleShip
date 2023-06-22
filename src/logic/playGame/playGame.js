2b1c9d39efb56116457a7a7c4d76f487bfaea255
J <59581793+daechi73@users.noreply.github.com>
2023-06-22 00:01:39 -0400

    setup for pc board testing

--- src/logic/playGame/playGame.js
+++ src/logic/playGame/playGame.js
@@ -1,15 +1,26 @@
 import gameBoard from "../gameBoard/gameBoard";
 import player from "../player/computer/player";
 import computer from "../player/computer/computer";
-import { choosingUI } from "../../render/renderUI";
+import renderUI from "../../render/renderUI/renderUI";
+import shipId from "../utility/shipId.js";
 
 const playGame = (playerName) => {
-  const playerBoard = gameBoard();
-  const player2Board = gameBoard();
+  const id = shipId();
+
+  const player1Board = gameBoard(id);
+  const player2Board = gameBoard(id);
   const player1 = player(playerName);
   const player2 = computer();
 
-  choosingUI(playerBoard);
+  deleteThisafter(player1Board);
+
+  renderUI(player1Board, player2Board);
+};
+
+const deleteThisafter = (player1Board) => {
+  player1Board.getShips().forEach((ship) => {
+    ship.setCoord([0, 0]);
+  });
 };
 
 export default playGame;
