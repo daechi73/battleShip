899b245d09584c4333893cc5c171e15690bb0cde
J <59581793+daechi73@users.noreply.github.com>
2023-06-24 01:02:02 -0400

    added player1,player2 objects as parameter

--- src/logic/utility/gameStartBtnEventListener.js
+++ src/logic/utility/gameStartBtnEventListener.js
@@ -1,10 +1,13 @@
 import { renderPlayer2Side } from "../../render/renderUI/renderPlayerSides";
+import cellRecieveAttackListener from "./cellRecieveAttack";
 
 const gameStartBtnEventListener = (
   gameStartBtn,
   player2Board,
   dragEventObject,
-  changePositionObject
+  changePositionObject,
+  player1,
+  player2
 ) => {
   gameStartBtn.addEventListener("click", () => {
     document.querySelector(".container-port").remove();
@@ -12,6 +15,7 @@ const gameStartBtnEventListener = (
     renderPlayer2Side(player2Board);
     dragEventObject.removeEvents();
     changePositionObject.removeEvents();
+    cellRecieveAttackListener(player2Board, player1);
   });
 };
 export default gameStartBtnEventListener;
