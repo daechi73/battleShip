f90193ec71867fa9b9e2516c025688011ded2058
J <59581793+daechi73@users.noreply.github.com>
2023-06-26 02:28:01 -0400

    When user clicks a cell, it marks x if miss, and h if hit

--- src/logic/utility/cellRecieveAttack.js
+++ src/logic/utility/cellRecieveAttack.js
@@ -7,16 +7,25 @@ const cellRecieveAttackListener = (player2Board, player1) => {
   });
 };
 const cellRecieveAttack = (e, player2Board, player1) => {
-  e.target.textContent = "x";
   // console.log(
   //   player2Board.alphaNumCoordToNumCoord(e.target.dataset.alphanumcoord)
   // );
-  const result = player1.makeMove(
-    player2Board,
-    player2Board.alphaNumCoordToNumCoord(e.target.dataset.alphanumcoord)
-  );
-  console.log(result);
-  player2Board.printBoard();
+  if (e.target.textContent === "") {
+    const result = player1.makeMove(
+      player2Board,
+      player2Board.alphaNumCoordToNumCoord(e.target.dataset.alphanumcoord)
+    );
+    // console.log(result);
+    if (result) {
+      if (result.shipSunk)
+        console.log(`You've sank a ${result.ship.getName()}`);
+      if (result.sunkAllShips)
+        console.log(`You've sank all ships! game over !`);
+      e.target.innerHTML = "H";
+      e.target.style.backgroundColor = "red";
+    } else e.target.innerHTML += "X";
+    player2Board.printBoard();
+  }
 };
 
 export default cellRecieveAttackListener;
