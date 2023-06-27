b0f0d505950175d3b4c43ca98315d825397da5df
J <59581793+daechi73@users.noreply.github.com>
2023-06-27 00:43:26 -0400

    if player1 attacks a cell successfully, player2(pc) attacks

--- src/logic/utility/playRound.js
+++ src/logic/utility/playRound.js
@@ -1,10 +1,11 @@
 import cellRecieveAttack from "./cellRecieveAttack";
 
-const playRound = () => {
+const playRound = (player1Board, player2Board, player1, player2) => {
   const player2Cells = document.querySelectorAll(".container-player2 .cell");
   player2Cells.forEach((cell) => {
     cell.addEventListener("click", (e) => {
-      cellRecieveAttack(e, player2Board, player1);
+      const player1Attack = cellRecieveAttack(player2Board, player1, e);
+      if (player1Attack) cellRecieveAttack(player1Board, player2);
     });
   });
 };
