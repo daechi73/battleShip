48ff19be4717c142b8c581a0a5ccc7013c5f1af0
J <59581793+daechi73@users.noreply.github.com>
2023-06-27 00:41:18 -0400

    returns getCoordToAttack; makeMove iterates until computer finds an unattacked coordination

--- src/logic/player/computer/computer.js
+++ src/logic/player/computer/computer.js
@@ -1,15 +1,23 @@
 const computer = () => {
   let coordToAttack;
-  const makeMove = (board) => {
-    const randomX = Math.floor(Math.random() * 10);
-    const randomY = Math.floor(Math.random() * 10);
-    coordToAttack = [randomX, randomY];
 
-    if (board.checkCellAttacked(coordToAttack) === false) {
-      return board.receiveAttack(coordToAttack);
-    }
+  const makeMove = (board) => {
+    let pass = false;
+    do {
+      const randomX = Math.floor(Math.random() * 10);
+      const randomY = Math.floor(Math.random() * 10);
+      coordToAttack = [randomX, randomY];
+
+      if (board.checkCellAttacked(coordToAttack) === false) {
+        pass = true;
+        return board.receiveAttack(coordToAttack);
+      }
+    } while (pass == false);
   };
-  return { name: "Computer", makeMove, coordToAttack };
+  const getCoordToAttack = () => {
+    return coordToAttack;
+  };
+  return { name: "Computer", makeMove, getCoordToAttack };
 };
 
 export default computer;
