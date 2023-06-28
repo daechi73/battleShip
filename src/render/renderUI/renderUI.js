6438efb6044d717345e29be35f5b1850339b4929
J <59581793+daechi73@users.noreply.github.com>
2023-06-27 20:19:49 -0400

    added .gameTitle

--- src/render/renderUI/renderUI.js
+++ src/render/renderUI/renderUI.js
@@ -4,6 +4,13 @@ import dragEvent from "../../logic/utility/dragDrop.js";
 import changePositionListener from "../../logic/utility/changePosition";
 
 const renderUI = (player1Board, player2Board, player1, player2) => {
+  const gameTitle = document.createElement("div");
+  gameTitle.textContent = "BattleShip";
+  gameTitle.classList.add("gameTitle");
+  document.body.insertBefore(
+    gameTitle,
+    document.querySelector(".container-main")
+  );
   renderPlayer1Side(player1Board);
   renderPort(player1Board.getShips());
   const dragEventObj = dragEvent(player1Board, player2Board, player1, player2);
