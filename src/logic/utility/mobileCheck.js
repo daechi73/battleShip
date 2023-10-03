62085bb2d5878a4257812ada2afcba8f3ef04c9e
J <59581793+daechi73@users.noreply.github.com>
2023-10-03 00:27:05 -0400

    checks if browser is loading in mobile device

--- src/logic/utility/mobileCheck.js
+++ src/logic/utility/mobileCheck.js
@@ -1,6 +1,6 @@
 const mobileCheck = () => {
+  let check = false;
   window.mobileCheck = function () {
-    let check = false;
     (function (a) {
       if (
         /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
@@ -12,8 +12,8 @@ const mobileCheck = () => {
       )
         check = true;
     })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
   };
+  return check;
 };
 
 export default mobileCheck;
