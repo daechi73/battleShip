9d45c1ffb011be8b17e3c60faed16952a28f60d8
J <59581793+daechi73@users.noreply.github.com>
2023-10-03 01:09:33 -0400

    added Listener to window event resize to change dimensions of ships and shipSubsets

--- src/render/renderUI/renderShip.js
+++ src/render/renderUI/renderShip.js
@@ -1,3 +1,8 @@
+const windowWidthValidator = () => {
+  if (window.innerWidth <= 650) return true;
+  else return false;
+};
+
 const renderShip = (ship) => {
   const boat = document.createElement("div");
   boat.classList.add(`ship`);
@@ -11,16 +16,35 @@ const renderShip = (ship) => {
       shipSubset.dataset.subset = `${i}`;
       //shipSubset.dataset.mainset = `${ship.getName()}`;
       //shipSubset.dataset.mainsetLength = `${ship.getLength()}`;
+
       shipSubset.style.cssText = `
         height:35px;
         width:36px;
       `;
+      window.addEventListener("resize", () => {
+        if (windowWidthValidator()) {
+          shipSubset.style.cssText = `
+          height:25px;
+          width:25px;
+        `;
+        }
+      });
+
       boat.appendChild(shipSubset);
     }
 
     boat.classList.add("horizontal");
+
     boat.style.width = `${ship.getLength() * 36}px`;
     boat.style.height = "35px";
+
+    window.addEventListener("resize", () => {
+      if (windowWidthValidator()) {
+        boat.style.width = `${ship.getLength() * 26}px`;
+        boat.style.height = "25px";
+      }
+    });
+
     //boat.innerHTML = `<div class="shipName">${ship.getName()}</div>`;
     //boat.textContent = `${ship.getName()}`;
 
@@ -34,15 +58,33 @@ const renderShip = (ship) => {
       shipSubset.dataset.subset = `${i}`;
       //shipSubset.dataset.mainset = `${ship.getName()}`;
       //shipSubset.dataset.mainsetLength = `${ship.getLength()}`;
+
       shipSubset.style.cssText = `
         height:36px;
         width:37px;
       `;
+
+      window.addEventListener("resize", () => {
+        if (windowWidthValidator()) {
+          shipSubset.style.cssText = `
+          height:26px;
+          width:27px;
+        `;
+        }
+      });
+
       boat.appendChild(shipSubset);
     }
     boat.style.width = "35px";
     boat.style.height = `${ship.getLength() * 36}px`;
     boat.dataset.length = `${ship.getLength()}`;
+
+    window.addEventListener("resize", () => {
+      if (windowWidthValidator()) {
+        boat.style.height = `${ship.getLength() * 26}px`;
+        boat.style.width = "25px";
+      }
+    });
 
     boat.dataset.position = `${ship.getPosition()}`;
   }
