34676012649d6490547b06f6986939cf70f09717
J <59581793+daechi73@users.noreply.github.com>
2023-10-03 17:50:00 -0400

    component runs if a ship is selected

--- src/logic/utility/clickDrop.js
+++ src/logic/utility/clickDrop.js
@@ -9,6 +9,7 @@ const clickDrop = (player1Board, player2Board, player1, player2) => {
 
   const selectShip = (e) => {
     if (e.target.classList.contains("shipSubset")) {
+      e.stopPropagation();
       shipSubset = e.target.dataset.subset;
       selected = e.target.parentNode;
       console.log(e.target);
@@ -16,50 +17,44 @@ const clickDrop = (player1Board, player2Board, player1, player2) => {
     }
   };
   const positionShip = (e) => {
-    console.log(e.target);
-    if (positionUtility().checkBoundary(e.target, selected)) {
-      if (positionUtility().checkCellAvailability(e.target, selected)) {
-        if (e.target.classList.contains("droppable")) {
-          let realCellToAppend;
-          if (selected.dataset.position === "horizontal") {
-            realCellToAppend = document.querySelector(
-              `[data-column="${
-                e.target.dataset.column - (shipSubset - 1)
-              }"][data-row="${e.target.dataset.row}"]`
-            );
-          } else {
-            const newRow =
-              e.target.dataset.row.charCodeAt(0) - (shipSubset - 1);
-            realCellToAppend = document.querySelector(
-              `[data-column="${
-                e.target.dataset.column
-              }"][data-row="${String.fromCharCode(newRow)}"]`
-            );
-          }
-          console.log(realCellToAppend);
-          if (selected.parentNode.classList.contains("cell"))
-            positionUtility().enableCell(selected.parentNode, selected);
+    if (selected) {
+      console.log(e.target);
+      if (positionUtility().checkBoundary(e.target, selected)) {
+        if (positionUtility().checkCellAvailability(e.target, selected)) {
+          if (e.target.classList.contains("droppable")) {
+            let realCellToAppend;
+            if (selected.dataset.position === "horizontal") {
+              realCellToAppend = document.querySelector(
+                `[data-column="${
+                  e.target.dataset.column - (shipSubset - 1)
+                }"][data-row="${e.target.dataset.row}"]`
+              );
+            } else {
+              const newRow =
+                e.target.dataset.row.charCodeAt(0) - (shipSubset - 1);
+              realCellToAppend = document.querySelector(
+                `[data-column="${
+                  e.target.dataset.column
+                }"][data-row="${String.fromCharCode(newRow)}"]`
+              );
+            }
+            if (selected.parentNode.classList.contains("cell"))
+              positionUtility().enableCell(selected.parentNode, selected);
 
-          selected.parentNode.removeChild(selected);
-          realCellToAppend.appendChild(selected);
+            selected.parentNode.removeChild(selected);
+            realCellToAppend.appendChild(selected);
 
-          player1Board.placeShip(
-            player1Board.findShip(selected.id),
-            realCellToAppend.dataset.alphanumcoord
-          );
-          player1Board.printBoard();
-          positionUtility().disableCell(realCellToAppend, selected);
-          selected = null;
-          if (player1Board.allShipsOnBoard() == true) {
-            console.log("allShipOnBoard");
-            gameStartBtnEvent(
-              player1Board,
-              player2Board,
-              moveEventObj,
-              changePositionObj,
-              player1,
-              player2
+            player1Board.placeShip(
+              player1Board.findShip(selected.id),
+              realCellToAppend.dataset.alphanumcoord
             );
+            player1Board.printBoard();
+            positionUtility().disableCell(realCellToAppend, selected);
+            selected = null;
+            if (player1Board.allShipsOnBoard() == true) {
+              console.log("allShipOnBoard");
+              gameStartBtnEvent(player1Board, player2Board, player1, player2);
+            }
           }
         }
       }
@@ -71,34 +66,20 @@ const clickDrop = (player1Board, player2Board, player1, player2) => {
     moveEventObj = moveEventObject;
     changePositionObj = changePositionObject;
     ships.forEach((ship) => {
-      ship.addEventListener("click", (e) => {
-        e.stopPropagation();
-        selectShip(e);
-      });
+      ship.addEventListener("click", selectShip);
     });
     cells.forEach((cell) => {
-      cell.addEventListener("click", (e) => {
-        if (selected) {
-          positionShip(e);
-        }
-      });
+      cell.addEventListener("click", positionShip);
     });
   };
 
   const removeEvents = () => {
     const ships = document.querySelectorAll(".ship");
     ships.forEach((ship) => {
-      ship.removeEventListener("click", (e) => {
-        e.stopPropagation;
-        selectShip(e);
-      });
+      ship.removeEventListener("click", selectShip);
     });
     cells.forEach((cell) => {
-      cell.removeEventListener("click", (e) => {
-        if (selected) {
-          positionShip(e);
-        }
-      });
+      cell.removeEventListener("click", positionShip);
     });
   };
   const recallDragEvents = () => {
