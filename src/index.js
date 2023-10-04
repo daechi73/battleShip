62efa6990cc631316a05b6d600852dd58e16c87e
J <59581793+daechi73@users.noreply.github.com>
2023-10-03 22:46:31 -0400

    restyled gameoverUi; changed color startPage & gameOverUI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import player from "./logic/player/computer/player";
+import gameOverUI from "./logic/UI/gameOverUI";
 import startUI from "./logic/UI/startUI";
 import UI from "./logic/UI/UI";
 
 // UI();    //for deving UI
 
 startUI();
+
+// gameOverUI(player("a"));
